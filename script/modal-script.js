7d7f61de9a4d495a8858861643623148f69142b9
Yevhenii_Chukavov <cork@ukr.net>
2021-04-27 20:55:44 +0300

    create blog

--- script/modal-script.js
+++ script/modal-script.js
@@ -1,7 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
     const requestPrice = document.querySelector('.modal__request-price');
+    const submitBlock = document.querySelector('.submit-block');
+    const modalSubmit = document.querySelector('.modal__submit');
+    const checkboxes = document.querySelectorAll('.check-wrapper > input');
+
     requestPrice.addEventListener('click', (event) => {
         event.preventDefault();
-        console.log('Нажали!');
+        submitBlock.classList.remove('modal-hidden');
+        modalSubmit.classList.remove('modal-hidden');
+        requestPrice.classList.add('modal-hidden');
+        checkboxes.forEach(item => {
+            item.classList.add('check-disabled');
+            item.disabled= 'true'
+        })
+        console.log('Нажали!', checkboxes);
     })
 })
