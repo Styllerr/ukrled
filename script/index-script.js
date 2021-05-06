d03993eeffc3e01e2a7766a82ebc994a5f3ee95d
Yevhenii_Chukavov <cork@ukr.net>
2021-05-06 21:36:57 +0300

    start adaptiv

--- script/index-script.js
+++ script/index-script.js
@@ -1,15 +1,36 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const menuList = document.getElementById('product-list');
+    const productList = document.querySelector('ul.product__navigation');
+    const burgerMenu = document.querySelector('.burger-menu');
+    const menuBody = document.querySelector('.header__navigation');
+
     const cardShowBtn = document.querySelector('.proposition__card-show-btn');
     const hiddenCard = document.querySelectorAll('.hidden-card')
+
+
+    const menuShow = () => {
+        menuBody.classList.toggle('header__navigation_hidden');
+        burgerMenu.classList.toggle('burger-menu_checked');
+
+    }
     cardShowBtn.addEventListener('click', () => {
         for (let i = 0; i < hiddenCard.length; i++) {
             hiddenCard[i].classList.toggle('proposition__card_hidden');
         }
-        if(cardShowBtn.innerText === 'Показать ещё') {
+        if (cardShowBtn.innerText === 'Показать ещё') {
             cardShowBtn.innerText = 'Скрыть'
-        }else {
+        } else {
             cardShowBtn.innerText = 'Показать ещё'
-            
-        } 
+
+        }
     })
+
+    menuList.addEventListener('click', () => {
+        if (window.innerWidth < 1024) {
+            productList.classList.toggle('product__navigation_hidden')
+        }
+        console.log(window.innerWidth)
+    })
+    
+    burgerMenu.addEventListener('click', menuShow);
 })
