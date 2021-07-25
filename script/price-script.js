056f9422cc5c9a99bd9a39ab6dd7e786674de34f
Yevhenii_Chukavov <cork@ukr.net>
2021-07-26 02:02:43 +0300

    add resolution in range

--- script/price-script.js
+++ script/price-script.js
@@ -20,104 +20,153 @@ if (request) {
 // Изменение значения и ед измерения ползунков
 
 const width = document.querySelector('.range__width'),
-height = document.querySelector('.range__height'),
-rangeUnits = document.querySelectorAll('.range__units'),
-widthRange = document.querySelector('.range__width-range'),
-heightRange = document.querySelector('.range__height-range'),
-metricSelect = document.querySelectorAll('input[name="metric"]');
+    height = document.querySelector('.range__height'),
+    rangeUnits = document.querySelectorAll('.range__units'),
+    widthRange = document.querySelector('.range__width-range'),
+    heightRange = document.querySelector('.range__height-range'),
+    metricSelect = document.querySelectorAll('input[name="metric"]'),
+    resolutionSelect = document.querySelector('select.request__pixel'),
+    resolution = { h: 320, v: 160 };
 let metric = 'metric-mm';
-widthRange.addEventListener('input', (event) => changeRange(event))
-heightRange.addEventListener('input', (event) => changeRange(event))
+widthRange.addEventListener('input', (event) => changeRange(event));
+heightRange.addEventListener('input', (event) => changeRange(event));
 metricSelect.forEach(element => {
-element.addEventListener('change', (event) => changeMetric(event))
+    element.addEventListener('change', (event) => changeMetric(event.target.id))
 });
+resolutionSelect.addEventListener('change', () => changeResolution())
 
 const changeRange = (event) => {
-if (event.target.className === 'range__width-range') {
-    width.innerText = widthRange.value
-} else if (event.target.className === 'range__height-range') {
-    height.innerText = heightRange.value
+    if (event.target.className === 'range__width-range') {
+        width.innerText = widthRange.value
+    } else if (event.target.className === 'range__height-range') {
+        height.innerText = heightRange.value
+    }
 }
+const changeMetric = (newMetric) => {
+    switch (metric) {
+        case "metric-mm":
+            if (newMetric === "metric-sm") {
+                let k = 10;
+                widthRange.setAttribute('step', resolution.h / k);
+                widthRange.value /= 10;
+                width.innerText = widthRange.value;
+                widthRange.setAttribute('max', '1000');
+                heightRange.setAttribute('step', resolution.v / k);
+                heightRange.value /= 10;
+                height.innerText = heightRange.value;
+                heightRange.setAttribute('max', '1000');
+                rangeUnits.forEach((item) => item.innerText = ' см');
+                metric = 'metric-sm';
+            } else if (newMetric === "metric-m") {
+                let k = 1000;
+                widthRange.setAttribute('step', resolution.h / k);
+                widthRange.value /= 1000;
+                width.innerText = widthRange.value;
+                widthRange.setAttribute('max', '10');
+                heightRange.setAttribute('step', resolution.v / k);
+                heightRange.value /= 1000;
+                height.innerText = heightRange.value;
+                heightRange.setAttribute('max', '10');
+                rangeUnits.forEach((item) => item.innerText = ' метр');
+                metric = 'metric-m';
+            } else if (newMetric === "resolution") {
+                widthRange.setAttribute('step', resolution.h);
+                heightRange.setAttribute('step', resolution.v);
+            }
+            break;
+        case "metric-sm":
+            if (newMetric === "metric-mm") {
+                let k = 1;
+                widthRange.setAttribute('max', '10000');
+                widthRange.value *= 10;
+                widthRange.setAttribute('step', resolution.h / k);
+                width.innerText = widthRange.value;
+                heightRange.setAttribute('max', '10000');
+                heightRange.value *= 10;
+                heightRange.setAttribute('step', resolution.v / k);
+                height.innerText = heightRange.value;
+                rangeUnits.forEach((item) => item.innerText = ' мм');
+                metric = 'metric-mm';
+            } else if (newMetric === "metric-m") {
+                let k = 1000;
+                widthRange.setAttribute('step', resolution.h / k);
+                widthRange.value /= 100;
+                width.innerText = widthRange.value;
+                widthRange.setAttribute('max', '10');
+                heightRange.setAttribute('step', resolution.v / k);
+                heightRange.value /= 100;
+                height.innerText = heightRange.value;
+                heightRange.setAttribute('max', '10');
+                rangeUnits.forEach((item) => item.innerText = ' метр');
+                metric = 'metric-m';
+            }else if (newMetric === "resolution") {
+                widthRange.setAttribute('step', resolution.h / 10);
+                heightRange.setAttribute('step', resolution.v / 10);
+            }
+            break;
+        case "metric-m":
+            if (newMetric === "metric-mm") {
+                let k = 1;
+                widthRange.setAttribute('max', '10000');
+                widthRange.value *= 1000;
+                widthRange.setAttribute('step', resolution.h / k);
+                width.innerText = widthRange.value;
+                heightRange.setAttribute('max', '10000');
+                heightRange.value *= 1000;
+                heightRange.setAttribute('step', resolution.v / k);
+                height.innerText = heightRange.value;
+                rangeUnits.forEach((item) => item.innerText = ' мм');
+                metric = 'metric-mm';
+            } else if (newMetric === "metric-sm") {
+                let k = 100;
+                widthRange.setAttribute('max', '1000');
+                widthRange.value *= 100;
+                widthRange.setAttribute('step', resolution.h / k);
+                width.innerText = widthRange.value;
+                heightRange.setAttribute('max', '1000');
+                heightRange.value *= 100;
+                heightRange.setAttribute('step', resolution.v / k);
+                height.innerText = heightRange.value;
+                rangeUnits.forEach((item) => item.innerText = ' см');
+                metric = 'metric-sm';
+            }else if (newMetric === "resolution") {
+                widthRange.setAttribute('step', resolution.h / 1000);
+                heightRange.setAttribute('step', resolution.v / 1000);
+            }
+            break;
+        default:
+            break;
+    }
 }
-const changeMetric = (event) => {
-let newMetric = event.target.id;
-switch (metric) {
-    case "metric-mm":
-        if (newMetric === "metric-sm") {
-            widthRange.setAttribute('step', '32');
-            widthRange.value /=  10;
-            width.innerText = widthRange.value;
-            widthRange.setAttribute('max', '1000');
-            heightRange.setAttribute('step', '16');
-            heightRange.value /=  10;
-            height.innerText = heightRange.value;
-            heightRange.setAttribute('max', '1000');
-            rangeUnits.forEach( (item) => item.innerText = ' см');
-            metric = 'metric-sm';
-        } else if (newMetric === "metric-m") {
-            widthRange.setAttribute('step', '0.32');
-            widthRange.value /= 1000;
-            width.innerText = widthRange.value;
-            widthRange.setAttribute('max', '10');
-            heightRange.setAttribute('step', '0.16');
-            heightRange.value /= 1000;
-            height.innerText = heightRange.value;
-            heightRange.setAttribute('max', '10');
-            rangeUnits.forEach( (item) => item.innerText = ' метр');
-            metric = 'metric-m';
-        }
-        break;
-    case "metric-sm":
-        if (newMetric === "metric-mm") {
-            widthRange.setAttribute('max', '10000');
-            widthRange.value *= 10;
-            widthRange.setAttribute('step', '320');
-            width.innerText = widthRange.value;
-            heightRange.setAttribute('max', '10000');
-            heightRange.value *= 10;
-            heightRange.setAttribute('step', '160');
-            height.innerText = heightRange.value;
-            rangeUnits.forEach( (item) => item.innerText = ' мм');
-            metric = 'metric-mm';
-        } else if (newMetric === "metric-m") {
-            widthRange.setAttribute('step', '0.32');
-            widthRange.value /= 100;
-            width.innerText = widthRange.value;
-            widthRange.setAttribute('max', '10');
-            heightRange.setAttribute('step', '0.16');
-            heightRange.value /= 100;
-            height.innerText = heightRange.value;
-            heightRange.setAttribute('max', '10');
-            rangeUnits.forEach( (item) => item.innerText = ' метр');
-            metric = 'metric-m';
-        }
-        break;
-    case "metric-m":
-        if (newMetric === "metric-mm") {
-            widthRange.setAttribute('max', '10000');
-            widthRange.value *= 1000;
-            widthRange.setAttribute('step', '320');
-            width.innerText = widthRange.value;
-            heightRange.setAttribute('max', '10000');
-            heightRange.value *= 1000;
-            heightRange.setAttribute('step', '160');
-            height.innerText = heightRange.value;
-            rangeUnits.forEach( (item) => item.innerText = ' мм');
-            metric = 'metric-mm';
-        } else if (newMetric === "metric-sm") {
-            widthRange.setAttribute('max', '1000');
-            widthRange.value *= 100;
-            widthRange.setAttribute('step', '32');
-            width.innerText = widthRange.value;
-            heightRange.setAttribute('max', '1000');
-            heightRange.value *= 100;
-            heightRange.setAttribute('step', '16');
-            height.innerText = heightRange.value;
-            rangeUnits.forEach( (item) => item.innerText = ' см');
-            metric = 'metric-sm';
-        }
-        break;
-    default:
-        break;
-}
+const changeResolution = () => {
+    switch (resolutionSelect.value) {
+        case "р10":
+            resolution.h = 320
+            resolution.v = 160
+            break;
+        case "р8":
+            resolution.h = 320
+            resolution.v = 160
+            break;
+        case "р6":
+            resolution.h = 320
+            resolution.v = 160
+            break;
+        case "р5":
+            resolution.h = 320
+            resolution.v = 160
+            break;
+        case "р4":
+            resolution.h = 320
+            resolution.v = 160
+            break;
+        case "р3":
+            resolution.h = 192
+            resolution.v = 192
+            break;
+
+        default:
+            break;
+    }
+    changeMetric("resolution")
 }
